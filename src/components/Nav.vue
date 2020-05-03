b308f626eb0fb13965464b28b357eb76fb6b09db
David Ackroyd <davidackroydmusic@gmail.com>
2020-05-03 13:54:25 +0000

    adding bootstrap[

---
+++ src/components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="navbar">
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'navbar',
+
+    data() {
+        return {
+        }
+    },
+
+    methods: {
+    },
+}
+
+</script>
