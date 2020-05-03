7dd4721485c6a15f46327a8cc1074374d861002a
David Ackroyd <dackroyd@aspidistra.com>
2020-05-03 14:11:43 +0000

    basic nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,13 @@
 <template>
-    <div id="navbar">
-        
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">PureBoard</a>
+
+        <ul class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Hot</a>
+            <a class="nav-item nav-link" href="#">New</a>
+            <a class="nav-item nav-link" href="#">Controversial</a>
+        </ul>
+    </nav>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
